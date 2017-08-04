498fdd463c33c467d9e5dd33f5668b0d62cc3cb4
Carlos Ferreira <carlosferreira@outlook.com>
2017-08-04 23:43:23 +0100

    Added way to fetch for bitcoin rpc information safely (hides password)

--- app/config.php
+++ app/config.php
@@ -4,7 +4,22 @@
 define('DEV_MODE', 1);
 
 // Global definitions - Change these to match your own Bitcoin Core information
-define('RPC_USER', 'carlos');
-define('RPC_PASSWORD', 'carlosFYIBitcoin201715thJuly');
+$bitcoin_config = file_get_contents('/home/carlos/.bitcoin/bitcoin.conf');
+$bitcoin_config = explode("\n", $bitcoin_config);
+
+$temp_array = array();
+foreach ($bitcoin_config as $config)
+{
+	if (strpos($config, '=') === false)
+	{
+		continue;
+	}
+	$bits = explode('=', $config);
+	$temp_array[$bits[0]] = $bits[1];
+}
+$bitcoin_config = $temp_array;
+
+define('RPC_USER', $bitcoin_config['rpcuser']);
+define('RPC_PASSWORD', $bitcoin_config['rpcpassword']);
 define('RPC_HOST', '127.0.0.1');
 define('RPC_PORT', '8332');
